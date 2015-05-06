8a1c152049b81a434d50ba2247360e29ca2cc9e8
Raphaël Doré <rdore@neomailbox.ch>
2015-05-05 21:12:05 -0400

    main complété

--- TutoratAppl/Program.cs
+++ TutoratAppl/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DataLayer.EfEntityFramework;
+using DataLayer.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +12,15 @@ namespace TutoratAppl
     class Program
     {
         static void Main(string[] args)
-        {
-            
-           // new TutorController(new EfEntityRepository<Tutor>());
+        {            
+            TutorController tutorController = new TutorController(new EfEntityRepository<Tutor>());
+            tutorController.ListAll();
+
+            HelpedStudentController helpStudentController = new HelpedStudentController(new EfEntityRepository<HelpedStudent>());
+            helpStudentController.ListAll();
+
+            TutoringSessionController tutoringSessionController = new TutoringSessionController(new EfEntityRepository<TutoringSession>());
+            tutoringSessionController.ListAll();
         }
     }
 }
