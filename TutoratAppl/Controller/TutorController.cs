021026f5745a3eb716edd51d742c51b538bbffc4
Raphaël Doré <rdore@neomailbox.ch>
2015-05-06 19:27:42 -0400

    formatage

--- TutoratAppl/Controller/TutorController.cs
+++ TutoratAppl/Controller/TutorController.cs
@@ -1,10 +1,7 @@
 ﻿using DataLayer;
 using DataLayer.Model;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TutoratAppl.View;
 using TutoratAppl.ViewModel;
 
@@ -22,50 +19,53 @@ namespace TutoratAppl.Controller
         public void ListAll()
         {
             //Imprimer le nom, prénom, courriel, total heures tutorat, passéess et à venir, réalisées et planifiées
-            
-            var list = IEntityRepository.GetAll().Select(
-                s => new TutorListVM { 
-                    Id = s.Id, 
-                    LastName = s.LastName, 
-                    FirstName = s.FirstName, 
-                    EmailAddress = s.EmailAddress 
+
+            var tutorList = IEntityRepository.GetAll().Select(
+                s => new TutorListVM
+                {
+                    Id = s.Id,
+                    LastName = s.LastName,
+                    FirstName = s.FirstName,
+                    EmailAddress = s.EmailAddress
                 });
 
-            TutorListView display = new TutorListView(list);
+            TutorListView display = new TutorListView(tutorList);
             display.Display();
         }
 
         public void ListAllWithWorkingHoursTotal()
         {
-
-            var list = IEntityRepository.GetAll().Select(s => 
-                new TutorListTotalTutorHoursVM { 
-                    LastName = s.LastName, 
-                    FirstName = s.FirstName, 
-                    EmailAddress = s.EmailAddress, 
-                    TotalTutoringHours = s.tutoringSessions.Sum( x => (int?) x.LenghtSession) ?? 0
+            var tutorList = IEntityRepository.GetAll().Select(s =>
+                new TutorListTotalTutorHoursVM
+                {
+                    LastName = s.LastName,
+                    FirstName = s.FirstName,
+                    EmailAddress = s.EmailAddress,
+                    TotalTutoringHours = s.tutoringSessions.Sum(x => (int?)x.LenghtSession) ?? 0
                 });
 
-            TutorListTotalTutorHoursView display = new TutorListTotalTutorHoursView(list);
+            TutorListTotalTutorHoursView display = new TutorListTotalTutorHoursView(tutorList);
 
             display.Display();
         }
 
         public void listAllFreeTutorOnDate(DateTime dateTime)
         {
-            var list = IEntityRepository.GetAll().Where( t => 
+            // On regarde si
+            var tutorList = IEntityRepository.GetAll().Where(t =>
                 t.tutoringSessions.Any(x =>
                     x.DateSession.Year == dateTime.Year
                     && x.DateSession.Month == dateTime.Month
                     && x.DateSession.Day == dateTime.Day)).Select(tu =>
-                        new TutorListVM {
+                        new TutorListVM
+                        {
                             Id = tu.Id,
                             LastName = tu.LastName,
                             FirstName = tu.FirstName,
                             EmailAddress = tu.EmailAddress
                         });
 
-            TutorListView display = new TutorListView(list);
+            TutorListView display = new TutorListView(tutorList);
             display.Display();
         }
     }
